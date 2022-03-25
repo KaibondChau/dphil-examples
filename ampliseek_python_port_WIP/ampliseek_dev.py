dcac61e86726e41f3dcf0d64855d79c5c4ba217d
kevAnalysis1 <kevinchau197@gmail.com>
2022-03-25 12:48:44 +0000

    added dirs for outputs/logs and started mapping options

--- ampliseek_python_port_WIP/ampliseek_dev.py
+++ ampliseek_python_port_WIP/ampliseek_dev.py
@@ -6,6 +6,7 @@ import argparse
 import gzip
 import subprocess
 import time
+import os
 from Bio import SeqIO
 
 
@@ -42,37 +43,53 @@ def validate_input(args):
         assert len(fasta_content) >= 1, 'No records found in reverse fq.gz file'
 
 
+# todo wrap below 2 chunks into if intermediate and logs wanted
+
 # get filename for writing files - currently relies on standard output from Illumina name_<read#>.fq.gz/.fastq.gz # todo could add as argument instead
 def get_filename(args): 
     name = args.forward_reads.split('_1')
     filename = (name[0].split('/'))[-1]
     return(filename)
+
+# get directories to write optional intermediate outputs and logs    
+def get_dirs(filename):
+    log_dir = str(os.getcwd() + "/ampliseek_work/" + filename +"/logs/")
+    output_dir = str(os.getcwd() + "/ampliseek_work/" + filename +"/outputs/")
+    cov_dir = str(os.getcwd() + "/ampliseek_work/" + filename +"/")
+    return(log_dir, output_dir, cov_dir)
+
+
+# make directories
+def prep_dirs(filename):
+    os.makedirs(str(os.getcwd() + "/ampliseek_work/" + filename +"/logs"))
+    os.makedirs(str(os.getcwd() + "/ampliseek_work/" + filename +"/outputs"))
     
+
 # todo consider bash.sh commands as params to input into universal function
 
 # Interleaves forward and reverse reads using reformat.sh
-def interleave_reads(args, filename):
+def interleave_reads(args, filename, log_dir, output_dir):
     reformat_command = [
         "reformat.sh",
         str("in1=" + args.forward_reads),
         str("in2=" + args.reverse_reads),
-        str("out=" + filename + '_interleaved.fq.gz')]
+        str("out=" + output_dir + filename + '_interleaved.fq.gz')]
     p_interleave = subprocess.run(
         reformat_command,
         stdout=subprocess.DEVNULL, # supress stdout
         stderr=subprocess.PIPE, # capture stderr
         universal_newlines=True) # capture as str not bytes (supersedes decode)
-    e = open(str(filename + '_interleave_stderr'), 'w')  # create output stderr file  
+    e = open(str(log_dir + filename + '_interleave_stderr'), 'w')  # create output stderr file  
     e.write(p_interleave.stderr)
     e.close()
     
 
 # Trims interleaved reads using bbduk2.sh and standard Illumina adapters
-def trim_reads(filename):
+def trim_reads(filename, log_dir, output_dir):
     bbduk_command= [
         "bbduk2.sh",
-        str("in="+ str(filename + '_interleaved.fq.gz')),
-        str("out="+ filename + '_trimmmed.fq.gz'),
+        str("in="+ str(output_dir + filename + '_interleaved.fq.gz')),
+        str("out="+ output_dir + filename + '_trimmmed.fq.gz'),
         "mink=6",
         "ktrim=r",
         "k=19",
@@ -86,17 +103,17 @@ def trim_reads(filename):
         stdout=subprocess.DEVNULL,
         stderr=subprocess.PIPE,
         universal_newlines=True)
-    e = open(str(filename + '_trim_stderr'), 'w')  # create output stderr file 
+    e = open(str(log_dir + filename + '_trim_stderr'), 'w')  # create output stderr file 
     e.write(p_bbduk.stderr)  
     e.close()  
 
 
 # Merge trimmed reads using bbmerge-auto.sh 
-def merge_reads(filename):
+def merge_reads(filename, log_dir, output_dir):
     bbmerge_command= [
         "bbmerge-auto.sh",
-        str("in="+ str(filename + '_trimmmed.fq.gz')),
-        str("out="+ filename + '_merged.fq.gz'),
+        str("in="+ str(output_dir + filename + '_trimmmed.fq.gz')),
+        str("out="+ output_dir + filename + '_merged.fq.gz'),
         "k=62",
         "extend2=50",
         "ecct"]
@@ -105,51 +122,53 @@ def merge_reads(filename):
         stdout=subprocess.DEVNULL,
         stderr=subprocess.PIPE,
         universal_newlines=True)
-    e = open(str(filename + '_merge_stderr'), 'w')  # create output stderr file 
+    e = open(str(log_dir + filename + '_merge_stderr'), 'w')  # create output stderr file 
     e.write(p_bbmerge.stderr)  
     e.close()  
 
 
 # Map merged reads against Ampliseq AMR panel targets using bbmapskimmer.sh semiperfect mode
-def map_reads(filename):
+def map_reads(filename, log_dir, output_dir):
     bbmap_command= [
         "bbmapskimmer.sh",
-        str("in="+ str(filename + '_merged.fq.gz')),
+        str("in="+ str(output_dir + filename + '_merged.fq.gz')),
         "out=stdout.sam",
         "ref=ampliseq_targets_only.fasta", # ampliseq AMR panel targets - argument for generalisation
         "ambig=all",
+        "minid=0.80", # added options for relaxed mapping (minid needs to be set as 1 below idfilter)
+        "idfilter=0.98", # arg here for 0.9 or 0.98
         "minscaf=73",
         "saa=f",
         "sam=1.3",
-        "semiperfectmode=t",
+        #"semiperfectmode=t", # comment out for relaxed mapping
         "int=f"]
     p_bbmap = subprocess.run(
         bbmap_command,
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
         universal_newlines=True)
-    e = open(str(filename + '_mapping_stderr'), 'w')  # create output stderr file 
+    e = open(str(log_dir + filename + '_mapping_stderr'), 'w')  # create output stderr file 
     e.write(p_bbmap.stderr)  
     e.close()  
-    o = open(str(filename + '_mapped.sam'), 'w')  # create output stdout file 
+    o = open(str(output_dir + filename + '_mapped.sam'), 'w')  # create output stdout file 
     o.write(p_bbmap.stdout)  
     o.close() 
 
 # Output coverage using pileup.sh
-def pileup_reads(filename):
+def pileup_reads(filename, log_dir, output_dir, cov_dir):
     pileup_command= [
         "pileup.sh",
-        str("in="+ str(filename + '_mapped.sam')),
+        str("in="+ str(output_dir + filename + '_mapped.sam')),
         "out=stdout"]
     p_pileup = subprocess.run(
         pileup_command,
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
         universal_newlines=True)
-    e = open(str(filename + '_pileup_stderr'), 'w')  # create output stderr file 
+    e = open(str(log_dir + filename + '_pileup_stderr'), 'w')  # create output stderr file 
     e.write(p_pileup.stderr)  
     e.close()  
-    o = open(str(filename + '_coverage.txt'), 'w')  # create output stdout file 
+    o = open(str(cov_dir + filename + '_coverage.txt'), 'w')  # create output stdout file 
     o.write(p_pileup.stdout)  
     o.close()  
 
@@ -157,22 +176,26 @@ def main_function():
     start = time.time()
     args = get_args()
     filename = get_filename(args)
+    log_dir, output_dir, cov_dir = get_dirs(filename)
     #validate_input(args) # todo works locally but not on analysis1
+    prep_dirs(filename)
     print("Interleaving reads...")
-    interleave_reads(args, filename)
+    interleave_reads(args, filename, log_dir, output_dir)
     print("Completed")
     print("Trimming reads...")
-    trim_reads(filename)
+    trim_reads(filename, log_dir, output_dir)
     print("Completed")
     print("Merging reads...")
-    merge_reads(filename)
+    merge_reads(filename, log_dir, output_dir)
     print("Completed")
     print("Mapping reads...")
-    map_reads(filename)
+    map_reads(filename, log_dir, output_dir)
     print("Completed")
     print("Pileup")
-    pileup_reads(filename)
+    pileup_reads(filename, log_dir, output_dir, cov_dir)
     end = time.time()
     print(str("All done in " + str((round(end - start, 2))) + " seconds"))
     
 main_function()
+
+
